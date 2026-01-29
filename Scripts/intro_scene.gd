ed168f6a3169b8fdcd23525bda97086e3c3312fa
Yanndri <140993349+Yanndri@users.noreply.github.com>
2026-01-29 22:03:35 +0800

    fixed unmerges

--- Scripts/intro_scene.gd
+++ Scripts/intro_scene.gd
@@ -1,9 +1,13 @@
 extends Node3D
 
-@export var menu_scene : PackedScene
+@export var main_scene : PackedScene
 
 func _ready() -> void:
 	await get_tree().create_timer(3).timeout
-	await %Lamp.flicker()
-	await get_tree().create_timer(1).timeout
-	get_tree().change_scene_to_packed(menu_scene)
+	await %Lamp.flicker(16)
+	%center_label.text = "Presents"
+	await %Lamp.flicker(8)
+	%center_label.text = "Dark Choices"
+	await %Lamp.flicker(32)
+	await get_tree().create_timer(0.5).timeout
+	get_tree().change_scene_to_packed(main_scene)
